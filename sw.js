221b4312816bd6614c2034f34f23fedda1003de2
mberishaj73-cyber <mberishaj73@gmail.com>
2026-02-22 21:03:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,38 @@
-const CACHE_NAME = 'cycle-v1';
-const ASSETS = ['./', './index.html', './style.css', './app.js'];
+const CACHE_NAME = 'cycle-flow-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+// Install event: Caches the new files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); // Forces the new service worker to take over immediately
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Activate event: Deletes the old cache (v1)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event: Serves files from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
