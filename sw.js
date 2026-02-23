8c7262395925ac7bac55c3b95b5bfb20da622693
mberishaj73-cyber <mberishaj73@gmail.com>
2026-02-23 18:47:04 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // CACHE_NAME is like a version number for your app's memory. 
 // If you update your CSS or JS, changing this to 'v3' tells the phone to refresh everything.
-const CACHE_NAME = 'cycle-flow-v4';
+const CACHE_NAME = 'cycle-flow-v5';
 
 // ASSETS is the list of files the phone should "save" so the app works without internet.
 const ASSETS = [
@@ -49,5 +49,6 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
